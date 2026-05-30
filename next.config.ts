61c5af3ac90e91664d67e2cd7e94791c2a483635
Nawel0310 <nbarbaro@frba.utn.edu.ar>
2026-05-30 19:54:32 -0300

    configuracion next.js para output

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+const basePath = "/notipago";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath,
+  assetPrefix: basePath,
+  images: {
+    loader: "custom",
+    loaderFile: "./image-loader.ts",
+  },
 };
 
 export default nextConfig;
